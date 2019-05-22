1648962d650c5c39fe4f64637d3a7b6ac55f7427
Bram Ceulemans <bramceulemans@me.com>
2019-05-23 01:37:36 +0200

    Cleaned up code some more

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module orpa
 
 go 1.12
 
-require ekyu.moe/leb128 v0.0.0-20171215192340-34c607808d8a
+require (
+	ekyu.moe/leb128 v0.0.0-20171215192340-34c607808d8a
+	github.com/dustin/go-humanize v1.0.0
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	github.com/olekukonko/tablewriter v0.0.1
+)
--- main.go
+++ main.go
@@ -3,155 +3,148 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"encoding/json"
 	"fmt"
+	"github.com/dustin/go-humanize"
+	"github.com/olekukonko/tablewriter"
 	"io/ioutil"
+	"os"
 	"path"
 	"strings"
 )
 
-// OSU! mode (Standard, Taiko, Catch the Beat, Mania)
-type Mode byte
-
-const (
-	// Hex values for all modes
-	ModeStandard Mode = 0x0
-	ModeTaiko    Mode = 0x1
-	ModeCTB      Mode = 0x2
-	ModeMania    Mode = 0x3
-	// Names for all modes
-	ModeStandardName = "osu!standard"
-	ModeTaikoName    = "osu!taiko"
-	ModeCTBName      = "osu!catch"
-	ModeManiaName    = "osu!mania"
-)
-
-func (m Mode) String() string {
-	switch m {
-	default:
-	case ModeStandard:
-		return ModeStandardName
-	case ModeTaiko:
-		return ModeTaikoName
-	case ModeCTB:
-		return ModeCTBName
-	case ModeMania:
-		return ModeManiaName
-	}
-	return ModeStandardName
-}
-
-func (m Mode) MarshalJSON() ([]byte, error) {
-	return json.Marshal(m.String())
-}
-
-// Replay data structure
-type Replay struct {
-	Mode              Mode
-	GameVersion       uint32
-	BeatMapHash       string
-	PlayerName        string
-	ReplayHash        string
-	Amount300s        uint16
-	Amount100s        uint16
-	Amount50s         uint16
-	AmountSpecial300s uint16
-	AmountSpecial100s uint16
-	AmountMisses      uint16
-	TotalScore        uint32
-	LongestCombo      uint16
-	IsPerfect         bool
-	Mods              uint32
-	HealthGraph       string
-	Timestamp         uint64
-	ReplayData        []byte
-	ScoreID           uint64
-}
+const ReplayDir = "replays"
 
 func main() {
-	const replayDir = "replays"
-	replays, e := ioutil.ReadDir(replayDir)
-	LogError(e)
-
-	fmt.Printf("Found %d replays\n\n", len(replays))
-	for _, replayFileInfo := range replays {
-		replayFile := replayFileInfo.Name()
-		if strings.HasPrefix(replayFile, ".") {
+	// Get a list of all replay files and print the amount that will be processed
+	replays := getReplayFiles(ReplayDir)
+	fmt.Printf("Processing %d replays...\n\n", len(replays))
+	// Set up the table writer
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"#", "Beatmap", "Difficulty", "Score", "Mods"})
+	table.SetAutoWrapText(false)
+	// Loop over all the replays
+	for k, v := range replays {
+		filepath := path.Join(ReplayDir, v)
+		replay := getReplay(filepath)
+		beatmaps := GetBeatmap(replay.BeatMapHash)
+		if len(beatmaps) < 1 {
 			continue
 		}
-		replayPath := path.Join(replayDir, replayFile)
-		replay := getReplay(replayPath)
-		beatmap := GetBeatmap(replay.BeatMapHash)
-		fmt.Printf("%s\n", beatmap[0].Artist)
+		beatmap := beatmaps[0]
+		// Append data to table writer
+		table.Append([]string{
+			fmt.Sprintf("%d", k),
+			fmt.Sprintf("%s - %s", beatmap.Artist, beatmap.Title),
+			beatmap.Version,
+			humanize.Comma(int64(replay.TotalScore)),
+			fmt.Sprintf("%b", replay.Mods),
+		})
 	}
+	// Render the table to console
+	table.Render()
+	// Print how many replays were succesfully processed.
+	fmt.Printf("\nSuccesfully processed %d replays.\n", table.NumLines())
+}
+
+func getReplayFiles(directory string) []string {
+	replays, e := ioutil.ReadDir(directory)
+	LogError(e)
+	files := make([]string, 0)
+	for _, v := range replays {
+		name := v.Name()
+		if strings.HasPrefix(name, ".") {
+			continue
+		}
+		files = append(files, v.Name())
+	}
+	return files
 }
 
 func getReplay(replayFile string) Replay {
-	replayBytes, e := ioutil.ReadFile(replayFile)
+	reader, e := getReplayReader(replayFile)
 	LogError(e)
+	// Make a new replay and read it data into the replay
+	replay := Replay{}
+	readReplay(&replay, reader)
+	return replay
+}
 
-	// Create reader
+func getReplayReader(replayFile string) (*bufio.Reader, error) {
+	replayBytes, e := ioutil.ReadFile(replayFile)
+	if e != nil {
+		return nil, e
+	}
 	buffer := bytes.NewReader(replayBytes)
 	reader := bufio.NewReader(buffer)
+	return reader, e
+}
 
+func readReplay(replay *Replay, reader *bufio.Reader) {
 	mode, e := reader.ReadByte()
 	LogError(e)
+	replay.Mode = Mode(mode)
 
 	version, e := ReadInteger(reader)
 	LogError(e)
+	replay.GameVersion = version
 
 	beatmapHash, e := ReadString(reader)
 	LogError(e)
+	replay.BeatMapHash = beatmapHash
+
 	playerName, e := ReadString(reader)
 	LogError(e)
+	replay.PlayerName = playerName
+
 	replayHash, e := ReadString(reader)
 	LogError(e)
+	replay.ReplayHash = replayHash
+
 	amount300s, e := ReadShort(reader)
 	LogError(e)
+	replay.Amount300s = amount300s
+
 	amount100s, e := ReadShort(reader)
 	LogError(e)
+	replay.Amount100s = amount100s
+
 	amount50s, e := ReadShort(reader)
 	LogError(e)
+	replay.Amount50s = amount50s
+
 	amountSpecial300s, e := ReadShort(reader)
 	LogError(e)
+	replay.AmountSpecial300s = amountSpecial300s
+
 	amountSpecial100s, e := ReadShort(reader)
 	LogError(e)
+	replay.AmountSpecial100s = amountSpecial100s
+
 	misses, e := ReadShort(reader)
 	LogError(e)
+	replay.AmountMisses = misses
+
 	score, e := ReadInteger(reader)
 	LogError(e)
+	replay.TotalScore = score
+
 	combo, e := ReadShort(reader)
 	LogError(e)
+	replay.LongestCombo = combo
+
 	isPerfect, e := ReadBoolean(reader)
 	LogError(e)
+	replay.IsPerfect = isPerfect
+
 	mods, e := ReadInteger(reader)
 	LogError(e)
+	replay.Mods = mods
+
 	healthGraph, e := ReadString(reader)
 	LogError(e)
+	replay.HealthGraph = healthGraph
+
 	timestamp, e := ReadLong(reader)
 	LogError(e)
-
-	replay := Replay{
-		Mode:              Mode(mode),
-		GameVersion:       version,
-		BeatMapHash:       beatmapHash,
-		PlayerName:        playerName,
-		ReplayHash:        replayHash,
-		Amount300s:        amount300s,
-		Amount100s:        amount100s,
-		Amount50s:         amount50s,
-		AmountSpecial300s: amountSpecial300s,
-		AmountSpecial100s: amountSpecial100s,
-		AmountMisses:      misses,
-		TotalScore:        score,
-		LongestCombo:      combo,
-		IsPerfect:         isPerfect,
-		Mods:              mods,
-		HealthGraph:       healthGraph,
-		Timestamp:         timestamp,
-	}
-
-	LogError(e)
-
-	return replay
+	replay.Timestamp = timestamp
 }
